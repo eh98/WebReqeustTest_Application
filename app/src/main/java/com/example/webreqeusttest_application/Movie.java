9463a5fb61c3a838ebe4ea325903bc9023645c14
eh98 <eunhee.k.98@gmail.com>
2020-12-02 15:35:40 +0900

    영화 정보

---
+++ app/src/main/java/com/example/webreqeusttest_application/Movie.java
@@ -0,0 +1,22 @@
+package com.example.webreqeusttest_application;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rank0ldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiInten;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
---
+++ app/src/main/java/com/example/webreqeusttest_application/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.example.webreqeusttest_application;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
